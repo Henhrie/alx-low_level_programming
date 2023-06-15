60a8b60ba0245102f54e64542818158020c68815
Henhrie <akinolahenryayobami@gmail.com>
2023-06-15 10:51:48 -0700

    6size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
  *    **/
 
 int main(void)
-{  
+{
 printf("Size of char: %lu byte(s)\n", sizeof(char));
 printf("Size of char: %lu byte(s)\n", sizeof(int));
 printf("Size of char: %lu byte(s)\n", sizeof(long int));
