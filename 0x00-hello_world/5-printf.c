8fd276fb442baf8b6b42fbc2957dd559ca17e31c
Henhrie <akinolahenryayobami@gmail.com>
2023-06-15 10:10:16 -0700

    5prints

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  *  * main - Entry point
- *   * format - Declaration of print f
+ *   * *format - Declaration of print f
  *    * const - Declaration of print f
  *     * printf - formatted string to the standard output 
- *     * Return: Ends program
- *      **/
+ *      * Return: Ends program
+ *       **/
 
 int printf(const char *format, ...);
 
