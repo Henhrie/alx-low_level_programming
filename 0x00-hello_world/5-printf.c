b25ad410886f043a6d174f0184eced6539d6bbe5
Henhrie <akinolahenryayobami@gmail.com>
2023-06-15 09:50:30 -0700

    5prints

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    *
+ *     * Return: Ends program
+ *      **/
+
+int printf(const char *format, ...);
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
