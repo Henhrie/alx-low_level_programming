e73ba912a97b7846efdbe330548f9fae57c45709
Henhrie <akinolahenryayobami@gmail.com>
2023-06-27 03:19:36 -0700

    Revstring

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,22 +9,23 @@
 
 void rev_string(char *s)
 {
-        int i, len, len1;
-        char temp;
+	int i, len, len1;
+	char temp;
 
-        len = 0;
-        len1 = 0;
+	len = 0;
+	len1 = 0;
 
-        while (s[len] != '\0')
-        {
-                len++;
-        }
+	while (s[len] != '\0')
+{
+	len++;
+	}
 
-        len1 = len - 1;
+	len1 = len - 1;
 
-        for (i = 0; i < len / 2; i++)
-        {
-                temp = s[i];
-                s[i] = s[len1];
-                s[len1--] = temp;        }
+	for (i = 0; i < len / 2; i++)
+{
+	temp = s[i];
+	s[i] = s[len1];
+	s[len1--] = temp;
+	}
 }
