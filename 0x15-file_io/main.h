10e89ff199ba9b42a8b07a87e751ec308a8bf732
Henhrie <akinolahenryayobami@gmail.com>
2023-08-08 07:39:05 +0800

    File_io

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,35 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - Read and print text from a file
+ * @filename: The name of the file
+ * @letters: The number of letters to read and print
+ * Return: The number of letters read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/**
+ * create_file - Create a file and write text to it
+ * @filename: The name of the file to create
+ * @text_content: The content to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content);
+
+/**
+ * append_text_to_file - Append text to an existing file
+ * @filename: The name of the file to append to
+ * @text_content: The content to append to the file
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* MAIN_H */
