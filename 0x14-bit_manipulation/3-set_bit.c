77f918d186df3c7e4fcb47a434ae233205ac8422
Chukwumary123 <chukwumaryogbonnaya1695@gmail.com>
2023-09-30 20:04:00 +0800

    new

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,8 +9,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 53 || !n)
-		return (-1);
-	(*n |= 1 << index);
-	return (1);
+if (index > 53 || !n)
+return (-1);
+(*n |= 1 << index);
+return (1);
 }
