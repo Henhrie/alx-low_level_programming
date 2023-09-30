b4699a273646369a3d93b2153cc5be5490188a54
Chukwumary123 <chukwumaryogbonnaya1695@gmail.com>
2023-09-30 19:28:30 +0800

    New

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,32 +8,31 @@
  */
 unsigned int binary_to_uint(const char *binary)
 {
-    unsigned int result = 0;
-    unsigned int place_value = 1;
-    int index;
+unsigned int result = 0;
+unsigned int place_value = 1;
+int index;
 
-    if (!binary)
-        return (0);
+if (!binary)
+return (0);
 
-    for (index = 0; binary[index]; index++)
-        ;
-    index--;
+for (index = 0; binary[index]; index++);
+index--;
 
-    for (; index >= 0; index--)
-    {
-        if (binary[index] == '0')
-        {
-            place_value *= 2;
-            continue;
-        }
-        else if (binary[index] == '1')
-        {
-            result += place_value;
-            place_value *= 2;
-            continue;
-        }
-        return (0);
-    }
+for (; index >= 0; index--)
+{
+if (binary[index] == '0')
+{
+place_value *= 2;
+continue;
+}
+else if (binary[index] == '1')
+{
+result += place_value;
+place_value *= 2;
+continue;
+}
+return (0);
+}
 
-    return (result);
+return (result);
 }
