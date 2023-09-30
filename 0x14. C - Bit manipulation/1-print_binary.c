e1e7584b54e1cdbace2626a827fb764d4db80ba0
Chukwumary123 <chukwumaryogbonnaya1695@gmail.com>
2023-09-30 17:30:14 +0800

    Bit

---
+++ 0x14. C - Bit manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of an unsigned long integer.
+ * @n: The number to be represented in binary.
+ */
+void print_binary(unsigned long int n)
+{
+    /* Base case: If n is greater than 1, continue recursion. */
+    if (n > 1)
+        print_binary(n >> 1);
+
+    /* Print the least significant bit (LSB) of n. */
+    _putchar((n & 1) ? '1' : '0');
+}
